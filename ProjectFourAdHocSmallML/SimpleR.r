8b6041dc4004e25b8a9bfcb12504c8988cc945ac
Dalton Fabian <dalton.s.fabian@gmail.com>
2021-11-09 13:54:46 -0600

    ad hoc example

---
+++ ProjectFourAdHocSmallML/SimpleR.r
@@ -0,0 +1,12 @@
+
+library(dplyr)
+library(RODBC)
+
+# code for connecting to an Azure SQL database
+# ...
+
+# code for training a forecasting model or gathering data for a visualization
+# ...
+
+# output/save the info
+# ...
